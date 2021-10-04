b8639374d6d0cafe924045d42e2af55760739707
MatanR <matan@salaryo.com>
2021-10-04 19:32:10 +0300

    graph view

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import GraphView from "./views/GraphView/GraphView";
+import PlayerInfoView from "./views/PlayerInfoView/PlayerInfoView";
+
+function App() {
+  return (
+    <div className="App">
+        <GraphView/>
+        <PlayerInfoView/>
+    </div>
+  );
+}
+
+export default App;
